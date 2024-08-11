e1430eecdc35bf7f0b369d406757d3a6702dbe57
unknown <cristianalcaraz@outlook.com>
2024-08-11 10:15:35 -0300

    entrega final

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -3,11 +3,26 @@ import { leerProductos, guardarProductos } from '../utils/products_utils.js';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  const products = leerProductos();
-  res.json(products);
-});
+router.get("/", async (req, res) => {
+  const { limit = 10, page = 1, sort = '', ...query } = req.query;
+  const sortManager = {
+    'asc': 1,
+    'desc': -1
+  }
+  const products = await productModel.paginate(
+    { ...query },
+    { 
+      limit,
+      page,
+      ...(sort && { sort: { price: sortManager[sort]} }),
+      customLabels: { docs: 'payload' }
+    })
 
+  res.json({
+    ...products,
+    status: 'success'
+  });
+});
 router.get('/:pid', (req, res) => {
   const products = leerProductos();
   const product = products.find(p => p.id === req.params.pid);
